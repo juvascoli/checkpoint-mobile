f7e3b1969ccdc5dc519f483077f5efa185ba0369
juvascoli <jvascoli6@gmail.com>
2025-03-11 11:29:37 -0300

    textInput & button

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Image, TextInput, Button } from 'react-native';
+import { useState } from 'react';
+import RenderizarDados from './src/Components/renderizarDados';
+ 
 export default function App() {
+  const [valorOriginal, setValorOriginal] = useState(0)
+  const [aumento, setAumento] = useState(0)
+  const [resultado,setResultado]=useState(0)
+
+ 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        style={styles.imagem}
+        source={require('./assets/calculator.jpg')}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder='Digite o valor original do produto: '
+        value={valorOriginal}
+        onChangeText={(valor) => setValorOriginal(valor)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder='Digite a porcentagem de aumento: '
+        maxLength={30}
+        value={aumento}
+        onChangeText={(valor) => setAumento(valor)}
+      />
+      <Button
+        title='Clique aqui para receber o resultado'
+        color={'#000'}
+        fontSize={20}
+        onPress={()=>setResultado(true)}
+      />
+      {resultado&&<RenderizarDados  valorOriginal= {valorOriginal} aumento={aumento} resultado={resultado}/>}
+ 
     </View>
   );
 }
-
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#b7cfeb',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  imagem: {
+    resizeMode: 'center',
+    width: 300,
+  },
+  input: {
+    backgroundColor: '#fff',
+    width: 300,
+    borderRadius: 10,
+    paddingLeft: 10,
+    fontSize: 15,
+    marginBottom: 20
+  }
 });
+ 
+ 
+
+ 
---
+++ src/Components/calculo.js
@@ -0,0 +1,9 @@
+
+export default function Calculo{
+    return(
+
+    )
+}
+
+
+
